687bd393cfc3650921be6afbdeabc6372813f3b0
Jonathan Prieto <jonathan.prieto@me.com>
2020-10-29 09:55:55 -0500

    Suscriber OK

--- Publisher/src/main/java/MessagingRabbitmqApplication.java
+++ Publisher/src/main/java/MessagingRabbitmqApplication.java
@@ -33,21 +33,6 @@ public class MessagingRabbitmqApplication {
         return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
     }
 
-    @Bean
-    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-                                             MessageListenerAdapter listenerAdapter) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(queueName);
-        container.setMessageListener(listenerAdapter);
-        return container;
-    }
-
-    @Bean
-    MessageListenerAdapter listenerAdapter(Receiver receiver) {
-        return new MessageListenerAdapter(receiver, "receiveMessage");
-    }
-
     public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(MessagingRabbitmqApplication.class, args).close();
     }
